29e05cadc810b7a933b18aa6c6c5d57958aacdb2
CLG01\15960 <SkyChen@chailease.com.tw>
2024-04-19 17:32:43 +0800

    feat:order頁面 part3

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,13 +1,17 @@
 import { SplitmediaTextComponent } from './../../shared/compoents/splitmedia-text/splitmedia-text.component';
-import { Component } from '@angular/core';
+import { Component,NO_ERRORS_SCHEMA } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [SplitmediaTextComponent],
+  imports: [
+    SplitmediaTextComponent,
+    TabViewModule,
+    ImageModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
-
 }
