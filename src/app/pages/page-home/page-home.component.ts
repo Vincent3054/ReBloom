6cce99b539d21304c8ae55376a04c9f415c8ba08
Vincent3054 <ok96305@gmail.com>
2024-05-08 03:01:16 +0800

    feat:home part 3

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-page-home',
@@ -8,4 +8,39 @@ import { Component} from '@angular/core';
   styleUrl: './page-home.component.scss'
 })
 export class PageHomeComponent {
+  PageHomeTopImgSrc = '../../../assets/images/home/768-top-img.png';
+  PageHomeBottomImgSrc = '../../../assets/images/home/768-bottom-img.png';
+
+  constructor() {
+    this.updateImageSource();
+  }
+
+  // 監聽視窗大小變化
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateImageSource();
+  }
+
+  // 根據螢幕寬度更新圖片來源
+  updateImageSource() {
+    if (window.innerWidth <= 400) {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/400-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/400-bottom-img.png';
+    } else if (window.innerWidth <= 500) {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/500-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/500-bottom-img.png';
+    } else if (window.innerWidth <= 600) {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/600-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/600-bottom-img.png';
+    } else if (window.innerWidth <= 700) {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/700-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/700-bottom-img.png';
+    } else if (window.innerWidth <= 768) {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/768-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/768-bottom-img.png';
+    } else {
+      this.PageHomeTopImgSrc = '../../../assets/images/home/768-top-img.png';
+      this.PageHomeBottomImgSrc = '../../../assets/images/home/768-bottom-img.png';
+    }
+  }
 }
