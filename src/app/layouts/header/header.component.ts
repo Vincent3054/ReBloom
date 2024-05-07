140d0bf97ed31686c53cc3374db5dc6a9b7a43a0
CLG01\15960 <SkyChen@chailease.com.tw>
2024-05-07 16:47:13 +0800

    feat:header part 1

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+ // 設定初始圖片來源
+  HeaderLogImgSrc = '../../../assets/images/layouts/1280-header-logo.png';
+//  topRightImgSrc = '../../../assets/images/home/1280-top-right-img.png';
 
+ constructor() {
+   this.updateImageSource();
+ }
+
+ // 監聽視窗大小變化
+ @HostListener('window:resize', ['$event'])
+ onResize(event:Event) {
+   this.updateImageSource();
+ }
+
+ // 根據螢幕寬度更新圖片來源
+ updateImageSource() {
+   if (window.innerWidth <= 768) {
+     this.HeaderLogImgSrc = '../../../assets/images/layouts/768-header-logo.png';
+   } else {
+     this.HeaderLogImgSrc = '../../../assets/images/layouts/1280-header-logo.png';
+   }
+ }
 }
