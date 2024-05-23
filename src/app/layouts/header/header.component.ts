b920b26015164bb4d8fc458432c619b9d4a40b45
Vincent3054 <ok96305@gmail.com>
2024-05-24 03:10:07 +0800

    feat:最終修正

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -21,7 +21,7 @@ export class HeaderComponent {
   STATIC_ROUTES = STATIC_ROUTES;
 
   constructor(private router: Router, private route: ActivatedRoute) {
-    this.updateImageSource();
+    // this.updateImageSource();
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
@@ -31,7 +31,7 @@ export class HeaderComponent {
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
-    this.updateImageSource();
+    // this.updateImageSource();
   }
 
   toggleMenu() {
@@ -42,35 +42,35 @@ export class HeaderComponent {
     this.isMenuOpen = false; // 關閉菜單
   }
 
-  updateImageSource() {
-    if (window.innerWidth <= 390) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/390-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/390-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/390-header-closed-hamburger.png';
-    } else if (window.innerWidth <= 400) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/400-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/400-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/400-header-closed-hamburger.png';
-    } else if (window.innerWidth <= 430) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/430-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/430-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/430-header-closed-hamburger.png';
-    } else if (window.innerWidth <= 500) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/500-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/500-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/500-header-closed-hamburger.png';
-    } else if (window.innerWidth <= 600) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/600-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/600-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/600-header-closed-hamburger.png';
-    } else if (window.innerWidth <= 768) {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/768-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/768-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/768-header-closed-hamburger.png';
-    } else {
-      this.HeaderLogoImgSrc = '../../../assets/images/layouts/1280-header-logo.png';
-      this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/768-header-hamburger.png';
-      this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/768-header-closed-hamburger.png';
-    }
-  }
+  // // updateImageSource() {
+  // //   if (window.innerWidth <= 390) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/390-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/390-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/390-header-closed-hamburger.png';
+  // //   } else if (window.innerWidth <= 400) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/400-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/400-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/400-header-closed-hamburger.png';
+  // //   } else if (window.innerWidth <= 430) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/430-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/430-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/430-header-closed-hamburger.png';
+  // //   } else if (window.innerWidth <= 500) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/500-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/500-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/500-header-closed-hamburger.png';
+  // //   } else if (window.innerWidth <= 600) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/600-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/600-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/600-header-closed-hamburger.png';
+  // //   } else if (window.innerWidth <= 768) {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/768-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/768-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/768-header-closed-hamburger.png';
+  // //   } else {
+  // //     this.HeaderLogoImgSrc = '../../../assets/images/layouts/1280-header-logo.png';
+  // //     this.HeaderHamburgerImgSrc = '../../../assets/images/layouts/768-header-hamburger.png';
+  // //     this.HeadercloseHamburgerImgSrc = '../../../assets/images/layouts/768-header-closed-hamburger.png';
+  // //   }
+  // }
 }
